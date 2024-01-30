582a25ab4461863028fd8b885b7f8dbce06eaf8b
Daiane Arruda <daianedasilvaarruda@outlook.com>
2024-01-29 21:26:58 -0300

    finalizando css

--- imc/src/Result.jsx
+++ imc/src/Result.jsx
@@ -1,11 +1,12 @@
 import './App';
+import styles from './App.module.css';
 
 
 function Result({ClassificacaoIMC , imc}){
     return(
-        <div>
-        <h2>Seu IMC é: {imc}</h2>
-        <h3>Classificação: {ClassificacaoIMC(imc)}</h3>
+        <div className={styles.divResul}>
+        <h2 className={styles.h2}>Seu IMC é: {imc}</h2>
+        <h3 className={styles.h3}>Classificação: {ClassificacaoIMC(imc)}</h3>
         </div>
     )
 }
